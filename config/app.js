d30811887f5128fa0b508a2faafbe7662dd673d4
Nathan Hahn <nhahn@andrew.cmu.edu>
2015-12-16 22:59:20 -0500

    Updated mongo auth

--- config/app.js
+++ config/app.js
@@ -1,6 +1,5 @@
 module.exports = {
-  database: process.env.MONGODB_URL || 'localhost/plantMinder',
-  ip: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-  port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  production: (process.env.OPENSHIFT_NODEJS_IP)? true:false
+database: (process.env.MONGODB_SERVICE_PORT)? 'mongodb://' + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@" + process.env.MONGODB_SERVICE_HOST + ":" + process.env.MONGODB_SERVICE_PORT + '/plants' : 'localhost/plants', 
+  port: 8080,
+  production: (process.env.MONGODB_SERVICE_PORT)? true:false
 }
